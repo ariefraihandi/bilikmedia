f059b2eb13ca7ddaa1e45bb2d44c9f7244bae4c0
ariefraihandi <raihandi93@gmail.com>
2024-09-29 21:45:25 +0700

    pp

--- resources/views/Download/download.blade.php
+++ resources/views/Download/download.blade.php
@@ -1,6 +1,5 @@
 @extends('Index.app')
-@push('header-script')  
-    <script src="https://alwingulla.com/88/tag.min.js" data-zone="104084" async data-cfasync="false"></script>
+@push('header-script')      
     {!! $socialAd->code !!}
     <style>
         .ad-banner {
